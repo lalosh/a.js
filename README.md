d88e2e4499cfc8177a7c7b18742fe41a1f6949ec
Louay Alosh <louayalosh17@gmail.com>
2017-08-03 18:45:51 +0300

    fix before/after

--- README.md
+++ README.md
@@ -1,2 +1,129 @@
 # a.js
-Relent the DOM by taking advantages of JS!
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>Relent the DOM by taking advantages of JS!</h1> 
+
+    
+    <div>
+        <h2>First</h2>
+        <code>&lt;script src="a.js"></script&gt;</code>
+        <p>
+            once you include the script 'a.js' in your page a variable called 'a' is used to
+            reference every function related to this library
+        </p>
+    </div>
+ 
+    <div>
+        <h2>Examples</h2>
+        <div>
+            <h2>Get the text contents:</h2>
+            <code>
+                <pre>
+                //use a CSS selector
+                text('#my_span');
+
+                //use an element
+                let element = (document.querySelector('#my_span'));
+                text(element); 
+                </pre>    
+            </code>
+        </div>
+        <div>
+            <h2>Select an Element:</h2>
+            <code>
+                <pre>
+                //return the inner text
+                a.select('#some_id').inside('div#another_Id').text
+
+                //return the element itself as an html node
+                a.select('#some_id').inside('div#another_Id').element    
+
+                //set an attribute
+                a.select('#some_id').inside('div#another_Id').attr.set('color')('white')
+
+                //get some attribute
+                a.select('#some_id').inside('div#another_Id').attr.get('class')    
+
+                //remove an attribute
+                a.select('#some_id').inside('div#another_Id').attr.remove('id')
+
+                //check if it has an attribute
+                a.select('#some_id').inside('div#another_Id').attr.hasA('id')
+        
+                </pre>
+            </code>
+            
+            <div class="notice">
+                <code>
+                <pre>
+                //providing and empty parameter to the function inside will
+                //select an element inside of the 'body' element of your document
+                a.select('#some_id').inside().....etc
+                </pre>
+                </code>
+            </div>
+        </div>
+        
+        <div>
+            <h2>HTML Attributes:</h2>
+            <code>
+                <pre>
+                let element = document.querySelector(div#some_id);
+
+                //set an attribute for element
+                attr(element).set('color')('white');
+
+                //get an attribute from element
+                attr(element).get('color');
+
+                //remove an attribute from element
+                attr(element).remove('background-color');
+
+                //check if the element has an attribute
+                attr(element).hasA('id');
+                </pre>
+            </code>
+        </div>
+       
+        <div>
+            <h2>Create Elements:</h2>
+            <code>
+                <pre>
+                //create h1 element with the inner text "super header" after/before the
+                //element with the id "#some_id"
+                a.create.element('h1').innerText('super header').after('#some_id')
+                a.create.element('h1').innerText('super header').before('#some_id')
+
+                //create a div elements contains the provided tags after/before the 
+                //div elements with a class called "cool_class" 
+                a.create.html('&lt;h2&gt;&lt;p&gt;amazing paragraph here&lt;/p&gt;&lt;/h2&gt;').after('div.cool_class')
+                a.create.html('&lt;h2&gt;&lt;p&gt;amazing paragraph here&lt;/p&gt;&lt;/h2&gt;').before('div.cool_class')
+                </pre>
+            </code>
+            
+            <div class="notice">
+                <pre>
+                empty param to after() and before() will result in inserting the 
+                element after the last element or before the first element respectively
+                </pre>
+            </div>
+        </div>
+
+        <div>
+            <h2></h2>
+            <code>
+                <pre>
+                    
+                </pre>
+            </code>
+        </div>
+        
+    </div>
+    
+</body>
+</html>
--- a.js
+++ a.js
@@ -1,24 +1,29 @@
 
 let a = (function a(){
 
+
     function text(input){  
+        
         if(typeof input == 'string')
             return text(document.querySelector(input));
+        
         else //it's a node element
             return input.textContent; 
     }
 
-    //select > inside
+
     function select(selectorString){
     
         let element = document.querySelector(selectorString);
         
         function inside(outerSelectorString){
+            
             if(!outerSelectorString)
                 outerSelectorString = 'body';
 
             let outerElement = document.querySelector(outerSelectorString);
             let innerElement = outerElement.querySelector(selectorString);
+            
             return {
                 text: text(innerElement),
                 element: (function returnElement(){return innerElement;})(),
@@ -32,60 +37,10 @@ let a = (function a(){
     }
 
 
-
-    function create(){
-
-        let wantedElement;
-
-        function element(elementTagName){
-            let element = document.createElement(elementTagName.toLowerCase());
-             wantedElement = element;
-             return {innerText: innerText};
-
-        }
-
-        function innerText(someText){
-            let textNode = document.createTextNode(someText);
-            wantedElement.appendChild(textNode);
-
-            return {
-                before: before,
-                after: after
-            };
-        }
-        function html(HTMLString){
-            let docFrag = document.createDocumentFragment();
-            let element = document.createElement('div');
-            
-            docFrag.appendChild(element);
-            docFrag.querySelector('div').innerHTML = HTMLString;
-            wantedElement = docFrag.querySelector('div');
-            return {
-                before: before,
-                after: after
-            };
-        }
-
-        function before(selectorString){
-            let query = document.querySelector(selectorString);
-            query.insertAdjacentHTML('beforebegin', wantedElement.outerHTML);
-        }
-
-        function after(selectorString){
-            let query = document.querySelector(selectorString);
-            query.insertAdjacentHTML('afterend', wantedElement.outerHTML);
-        }
-
-        return{
-            element: element,
-            html: html,
-        }
-    }
-
     function attr(element){
 
         function set(name){
-            return function(value){
+            return function takeValue(value){
                 element.setAttribute(name, value);
             }
         }
@@ -101,17 +56,92 @@ let a = (function a(){
         function hasA(name){
             return element.hasAttribute(name);
         }
+        
         return {
             set: set,
             get: get,
             remove: remove,
-            hasA: hasA,
+            hasA: hasA
         }
     }
+
+
+    function create(){
+
+        let wantedElement;
+
+        function element(elementTagName){
+            
+            let element = document.createElement(elementTagName.toLowerCase());
+            wantedElement = element;
+           
+            return {
+                innerText: innerText
+            };
+        }
+
+        function innerText(someText){
+            
+            let textNode = document.createTextNode(someText);
+            wantedElement.appendChild(textNode);
+
+            return {
+                before: before,
+                after: after
+            };
+        }
+
+        function html(HTMLString){
+        
+            let docFrag = document.createDocumentFragment();
+            let element = document.createElement('div');
+            
+            docFrag.appendChild(element);
+            docFrag.querySelector('div').innerHTML = HTMLString;
+            wantedElement = docFrag.querySelector('div');
+        
+            return {
+                before: before,
+                after: after
+            };
+        }
+
+        function before(selectorString){
+            
+            let query;
+
+            if(!selectorString)
+                query = document.body.firstElementChild;
+            else
+                query = document.querySelector(selectorString);
+
+            query.insertAdjacentHTML('beforebegin', wantedElement.outerHTML);
+        }
+
+        function after(selectorString){
+           
+            let query;
+
+            if(!selectorString)
+                query = document.body.lastElementChild;
+            else
+                query = document.querySelector(selectorString);
+            
+            query.insertAdjacentHTML('afterend', wantedElement.outerHTML);
+        }
+
+        return{
+            element: element,
+            html: html
+        }
+    }
+
+    
     return{
         select: select,
         create: create(),
         text: text,
         attr: attr,
     }
+
 })();
